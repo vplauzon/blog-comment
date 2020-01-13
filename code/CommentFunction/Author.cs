187f56db3c656039a20299ac53ee369c504b38f5
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2020-01-13 15:21:00 -0800

    Propagate Uri to author

--- code/CommentFunction/Author.cs
+++ code/CommentFunction/Author.cs
@@ -1,8 +1,10 @@
-﻿namespace SearchFunction
+﻿using System;
+
+namespace SearchFunction
 {
     internal class Author
     {
-        public Author(string name, string? url)
+        public Author(string name, Uri? url)
         {
             Name = name;
             Url = url;
@@ -10,6 +12,6 @@
 
         public string Name { get; }
         
-        public string? Url { get; }
+        public Uri? Url { get; }
     }
 }
